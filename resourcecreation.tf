3ac0bf8d7ea3e617d79d3f97abc4b8eee69a1396
Kanth052016git <19247345+Kanth052016git@users.noreply.github.com>
2019-07-13 16:24:53 +0530

    Add files via upload

---
+++ resourcecreation.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+region="us-west-2"
+}
+
+resource "aws_instance" "myfirstec2" {
+ami="ami-0f2176987ee50226e"
+instance_type="t2.micro"
+}
+
+resource "aws_s3_bucket" "myfirstbkt125" {
+bucket="myfirstbucket145"
+acl="private"
+}
