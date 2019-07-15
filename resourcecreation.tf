2e2296ca2e3cbec2027da6ba128b354221696018
Kanth052016git <19247345+Kanth052016git@users.noreply.github.com>
2019-07-15 14:07:37 +0530

    Update resourcecreation.tf

--- resourcecreation.tf
+++ resourcecreation.tf
@@ -2,12 +2,12 @@ provider "aws" {
 region="us-west-2"
 }
 
-resource "aws_instance" "myfirstec3" {
+resource "aws_instance" "myfirstec2" {
 ami="ami-0f2176987ee50226e"
 instance_type="t2.micro"
 }
 
-resource "aws_s3_bucket" "myfirstbkt125" {
-bucket="myfirstbucket145"
+resource "aws_s3_bucket" "myfirstbkt135" {
+bucket="myfirstbucket155"
 acl="private"
 }
